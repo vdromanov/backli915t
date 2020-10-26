ad83ab3c8317576146796f6fdbb491850b4b1e45
vdromanov <vdromanov@yahoo.com>
2020-10-26 22:09:55 +0300

    improved cli

--- cmd/i915-pwm-control/main.go
+++ cmd/i915-pwm-control/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 	"strings"
 
 	"github.com/vdromanov/i915-pwm-control/cmd/i915-pwm-control/regs"
@@ -18,18 +20,36 @@ func main() {
 	}
 
 	// Flags for both pwm and bl modes. Only one flag is allowed at the moment
-	incPointer := flag.Int("inc", 0xFFFFFFFF, "Increment value")
-	decPointer := flag.Int("dec", 0xFFFFFFFF, "Decrement value")
-	setPointer := flag.Int("set", 0xFFFFFFFF, "Set value")
-	flag.Parse()
+	perModeArgs := flag.NewFlagSet("", flag.ExitOnError)
+	incPointer := perModeArgs.Int("inc", 0xFFFFFFFF, "increment value")
+	decPointer := perModeArgs.Int("dec", 0xFFFFFFFF, "decrement value")
+	setPointer := perModeArgs.Int("set", 0xFFFFFFFF, "explicitly set value")
+
+	perModeArgs.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage:\n\t%s <mode> -<key> <value>\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Allowed modes are:\n\tpwm - adjusting frequency in Hz\n\tbl - adjusting brightness in %%\n")
+		fmt.Fprintf(os.Stderr, "Allowed keys are:\n")
+		perModeArgs.VisitAll(func(f *flag.Flag) { fmt.Fprintf(os.Stderr, "\t-%s - %v\n", f.Name, f.Usage) })
+		fmt.Fprintf(os.Stderr, "\nExample:\n\t%s pwm -inc 500\n", os.Args[0])
+	}
+
+	if len(os.Args) < 2 { // TODO: apply config in this case
+		perModeArgs.Usage()
+		os.Exit(-1)
+	}
+	mode := os.Args[1]
+
+	perModeArgs.Parse(os.Args[2:])
 
 	setFlags := make(map[string]bool) // Inc/Dec and Set modes are using different funcs => storing info, was the flag a Set mode or not
 	var newVal int
 	var actualFlag string
 
-	flag.Visit(func(f *flag.Flag) { setFlags[f.Name] = (f.Name == "set"); actualFlag = f.Name }) // Iterating over all explicitly set flags
-	if len(setFlags) > 1 {                                                                       // Only one flag is allowed per mode
-		log.Fatalln("Only one flag per option is allowed")
+	perModeArgs.Visit(func(f *flag.Flag) { setFlags[f.Name] = (f.Name == "set"); actualFlag = f.Name }) // Iterating over all explicitly set flags
+	if len(setFlags) != 1 {                                                                             // Only one flag is allowed per mode
+		fmt.Fprintf(os.Stderr, "Got keys: %v\nOnly one key is allowed!\n\n", perModeArgs.Args())
+		perModeArgs.Usage()
+		os.Exit(-1)
 	}
 
 	switch actualFlag { // Could increment/decrement/explicitly set value
@@ -43,12 +63,12 @@ func main() {
 
 	blcRegVal := regs.ReadReg(regs.BLC_PWM_PCH_CTL2_REG)
 
-	switch flag.Arg(0) { // Working mode select
+	switch mode { // Working mode select
 	case "pwm":
 		log.Println("Have chosen PWM mode")
-		if setFlags[actualFlag] { // Setting mode
+		if setFlags[actualFlag] { // Explicitly setting a new value
 			setFrequency(newVal, &blcRegVal)
-		} else { // Changing mode
+		} else { // Changing existing value
 			changeFrequency(newVal, &blcRegVal)
 		}
 	case "bl":
@@ -59,7 +79,9 @@ func main() {
 			changeBacklightPercent(newVal, &blcRegVal)
 		}
 	default:
-		log.Fatalln("Choose operating mode from bl/pwm") // TODO: Flag's print usage
+		fmt.Fprintf(os.Stderr, "Incorrect mode %s has provided!\n\n", mode)
+		perModeArgs.Usage()
+		os.Exit(-1)
 	}
 
 }
