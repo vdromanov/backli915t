5918019fbdfcfbdbe6b6ab72673a801f6a3c330f
vdromanov <vdromanov@yahoo.com>
2020-10-29 02:18:15 +0300

    correct errors handling in config part

--- cmd/backli915t/config.go
+++ cmd/backli915t/config.go
@@ -2,45 +2,47 @@ package main
 
 import (
 	"encoding/json"
-	"log"
+	"fmt"
 	"os"
 )
 
+// Config stores PWM frequency and brightness level, defined by user
 type Config struct {
 	BacklightPercent int `json:"bl"`
 	PwmFrequency     int `json:"pwm"`
 }
 
-func LoadConfig(fpath string) Config {
-	config := Config{ // Default values
+// LoadConfig initiallizes a Config struct with values, stored in json (or a fallback-ones)
+func LoadConfig(fpath string) (Config, error) {
+	config := Config{ // A fallback values
 		BacklightPercent: 30,
 		PwmFrequency:     2000,
 	}
 
 	fp, err := os.Open(fpath)
 	defer fp.Close()
-	if err == nil {
-		parser := json.NewDecoder(fp)
-		err := parser.Decode(&config)
-		if err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		log.Println(err.Error())
+	if err != nil {
+		return config, fmt.Errorf("open %s", fpath, err)
 	}
-	return config
+	parser := json.NewDecoder(fp)
+	if err := parser.Decode(&config); err != nil {
+		return config, fmt.Errorf("json parsing from %s", fpath, err)
+	} else {
+		return config, nil
+	}
 }
 
-func DumpConfig(config *Config, fpath string) {
+// DumpConfig writes actual config to a json
+func DumpConfig(config *Config, fpath string) error {
 	fp, err := os.Create(fpath)
 	defer fp.Close()
-	if err == nil {
-		writer := json.NewEncoder(fp)
-		err := writer.Encode(config)
-		if err != nil {
-			log.Fatalln(err)
-		}
+	if err != nil {
+		return fmt.Errorf("writing to %s", fpath, err)
+	}
+	writer := json.NewEncoder(fp)
+	if err := writer.Encode(config); err != nil {
+		return fmt.Errorf("encoding to json %v", *config, err)
 	} else {
-		log.Fatalln(err)
+		return nil
 	}
 }
